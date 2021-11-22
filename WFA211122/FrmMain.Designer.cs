0215afad18361a09a0ee37a7493050b3aafd3fd8
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2021-11-22 14:43:55 +0100

    jhvhj

--- WFA211122/FrmMain.Designer.cs
+++ WFA211122/FrmMain.Designer.cs
@@ -34,6 +34,20 @@
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpSzul = new System.Windows.Forms.DateTimePicker();
+            this.tbNev = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtpOltasDatum = new System.Windows.Forms.DateTimePicker();
+            this.tbElsoOraOra = new System.Windows.Forms.TextBox();
+            this.tbElsoOraPerc = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbOltasOra = new System.Windows.Forms.TextBox();
+            this.tbOltasPerc = new System.Windows.Forms.TextBox();
+            this.btnHozzaad = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,7 +63,7 @@
             this.Column5});
             this.dgv.Location = new System.Drawing.Point(12, 12);
             this.dgv.Name = "dgv";
-            this.dgv.Size = new System.Drawing.Size(886, 311);
+            this.dgv.Size = new System.Drawing.Size(1032, 311);
             this.dgv.TabIndex = 0;
             // 
             // Column1
@@ -77,19 +91,149 @@
             this.Column5.HeaderText = "első óra";
             this.Column5.Name = "Column5";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 439);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Születési dátum:";
+            // 
+            // dtpSzul
+            // 
+            this.dtpSzul.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpSzul.Location = new System.Drawing.Point(184, 434);
+            this.dtpSzul.Name = "dtpSzul";
+            this.dtpSzul.Size = new System.Drawing.Size(200, 26);
+            this.dtpSzul.TabIndex = 2;
+            // 
+            // tbNev
+            // 
+            this.tbNev.Location = new System.Drawing.Point(184, 376);
+            this.tbNev.Name = "tbNev";
+            this.tbNev.Size = new System.Drawing.Size(200, 26);
+            this.tbNev.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(126, 379);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Név:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(494, 437);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Első óra:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(515, 379);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Oltás:";
+            // 
+            // dtpOltasDatum
+            // 
+            this.dtpOltasDatum.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpOltasDatum.Location = new System.Drawing.Point(592, 376);
+            this.dtpOltasDatum.Name = "dtpOltasDatum";
+            this.dtpOltasDatum.Size = new System.Drawing.Size(200, 26);
+            this.dtpOltasDatum.TabIndex = 2;
+            // 
+            // tbElsoOraOra
+            // 
+            this.tbElsoOraOra.Location = new System.Drawing.Point(592, 434);
+            this.tbElsoOraOra.Name = "tbElsoOraOra";
+            this.tbElsoOraOra.Size = new System.Drawing.Size(36, 26);
+            this.tbElsoOraOra.TabIndex = 3;
+            // 
+            // tbElsoOraPerc
+            // 
+            this.tbElsoOraPerc.Location = new System.Drawing.Point(653, 434);
+            this.tbElsoOraPerc.Name = "tbElsoOraPerc";
+            this.tbElsoOraPerc.Size = new System.Drawing.Size(36, 26);
+            this.tbElsoOraPerc.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(634, 439);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = ":";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(861, 381);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 20);
+            this.label6.TabIndex = 1;
+            this.label6.Text = ":";
+            // 
+            // tbOltasOra
+            // 
+            this.tbOltasOra.Location = new System.Drawing.Point(819, 376);
+            this.tbOltasOra.Name = "tbOltasOra";
+            this.tbOltasOra.Size = new System.Drawing.Size(36, 26);
+            this.tbOltasOra.TabIndex = 3;
+            // 
+            // tbOltasPerc
+            // 
+            this.tbOltasPerc.Location = new System.Drawing.Point(880, 376);
+            this.tbOltasPerc.Name = "tbOltasPerc";
+            this.tbOltasPerc.Size = new System.Drawing.Size(36, 26);
+            this.tbOltasPerc.TabIndex = 3;
+            // 
+            // btnHozzaad
+            // 
+            this.btnHozzaad.Location = new System.Drawing.Point(130, 504);
+            this.btnHozzaad.Name = "btnHozzaad";
+            this.btnHozzaad.Size = new System.Drawing.Size(827, 54);
+            this.btnHozzaad.TabIndex = 4;
+            this.btnHozzaad.Text = "Hozzáad";
+            this.btnHozzaad.UseVisualStyleBackColor = true;
+            this.btnHozzaad.Click += new System.EventHandler(this.BtnHozzaad_Click);
+            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(910, 554);
+            this.ClientSize = new System.Drawing.Size(1056, 598);
+            this.Controls.Add(this.btnHozzaad);
+            this.Controls.Add(this.tbOltasPerc);
+            this.Controls.Add(this.tbOltasOra);
+            this.Controls.Add(this.tbElsoOraPerc);
+            this.Controls.Add(this.tbElsoOraOra);
+            this.Controls.Add(this.tbNev);
+            this.Controls.Add(this.dtpOltasDatum);
+            this.Controls.Add(this.dtpSzul);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dgv);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmMain";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Load += new System.EventHandler(this.FrmMain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -101,6 +245,20 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpSzul;
+        private System.Windows.Forms.TextBox tbNev;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dtpOltasDatum;
+        private System.Windows.Forms.TextBox tbElsoOraOra;
+        private System.Windows.Forms.TextBox tbElsoOraPerc;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbOltasOra;
+        private System.Windows.Forms.TextBox tbOltasPerc;
+        private System.Windows.Forms.Button btnHozzaad;
     }
 }
 
--- WFA211122/FrmMain.cs
+++ WFA211122/FrmMain.cs
@@ -21,10 +21,20 @@ namespace WFA211122
                 "Server = (localdb)\\MSSQLLocalDB;" +
                 "Database = datumok;";
             InitializeComponent();
+            dtpOltasDatum.CustomFormat = "MMMM dd.";
+            dtpSzul.MaxDate = DateTime.Now;
+            dtpOltasDatum.MinDate = new DateTime(2020, 03, 16);
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void FrmMain_Load(object sender, EventArgs e)
         {
+            FillDgv();
+        }
+
+        private void FillDgv()
+        {
+            dgv.Rows.Clear();
+
             using (var c = new SqlConnection(ConnectionString))
             {
                 c.Open();
@@ -35,13 +45,53 @@ namespace WFA211122
                 while (reader.Read())
                 {
                     dgv.Rows.Add(
-                        reader.GetInt32(0) * 10,
+                        reader.GetInt32(0),
                         reader["nev"],
                         reader.GetDateTime(2).ToString("yyyy-MM-dd"),
                         reader.GetDateTime(3).ToString("MMM dd, HH:mm"),
                         reader.GetTimeSpan(4));
                 }
             }
+        }
+
+        private void BtnHozzaad_Click(object sender, EventArgs e)
+        {
+            string nev = tbNev.Text;
+            string szul = dtpSzul.Value.ToString("yyyy-MM-dd");
+            string oltas = $"{dtpOltasDatum.Value.ToString("yyyy-MM-dd")} {tbOltasOra.Text}:{tbOltasPerc.Text}";
+            string elsoOra = $"{tbElsoOraOra.Text}:{tbElsoOraPerc.Text}";
+
+            using (var c = new SqlConnection(ConnectionString))
+            {
+                c.Open();
+                var command = new SqlCommand(
+                    "INSERT INTO emberek VALUES " +
+                    $"('{nev}', '{szul}', '{oltas}', '{elsoOra}');", c);
+
+                try
+                {
+                    string execeptionMessage = "";
+
+                    if (string.IsNullOrWhiteSpace(nev)) execeptionMessage += "üresen maradt a név mező\n";
+                    if (!int.TryParse(tbOltasOra.Text, out _)) execeptionMessage += "nem jó az oltás óra\n";
+                    if (!int.TryParse(tbOltasPerc.Text, out _)) execeptionMessage += "nem jó az oltás perc\n";
+                    //if (!int.TryParse(tbElsoOraOra.Text, out _)) throw new Exception();
+                    //if (!int.TryParse(tbElsoOraPerc.Text, out _)) throw new Exception();
+
+                    if (!string.IsNullOrEmpty(execeptionMessage))
+                        throw new Exception(execeptionMessage);
+
+
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+
+            FillDgv();
         }
     }
 }
